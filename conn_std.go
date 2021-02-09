e320a409f144c7ce6488e659f7555ca0731b4954
lesismal <40462947+lesismal@users.noreply.github.com>
2021-02-09 16:49:02 +0800

    - fix deadlock

--- conn_std.go
+++ conn_std.go
@@ -32,10 +32,8 @@ func (c *Conn) Hash() int {
 
 // Read wraps net.Conn.Read
 func (c *Conn) Read(b []byte) (int, error) {
-	c.mux.Lock()
 	if c.g.onRead != nil {
 		n, err := c.g.onRead(c, b)
-		c.mux.Unlock()
 		return n, err
 	}
 
@@ -46,7 +44,6 @@ func (c *Conn) Read(b []byte) (int, error) {
 		}
 		c.Close()
 	}
-	c.mux.Unlock()
 	return nread, err
 }
 
