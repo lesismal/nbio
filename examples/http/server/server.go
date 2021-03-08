632b7b66fdf0703d5fa81513e9a7ee829c4051b6
lesismal <40462947+lesismal@users.noreply.github.com>
2021-03-08 12:29:01 +0800

    - update http server example

--- examples/http/server/server.go
+++ examples/http/server/server.go
@@ -5,9 +5,15 @@ import (
 	"io"
 	"net/http"
 	"runtime"
+	"sync/atomic"
 	"time"
 
 	"github.com/lesismal/nbio/nbhttp"
+)
+
+var (
+	qps   uint64 = 0
+	total uint64 = 0
 )
 
 func onEcho(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +23,7 @@ func onEcho(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.Write([]byte(time.Now().Format("20060102 15:04:05")))
 	}
+	atomic.AddUint64(&qps, 1)
 }
 
 func main() {
@@ -24,8 +31,35 @@ func main() {
 	mux.HandleFunc("/echo", onEcho)
 
 	g := nbhttp.NewServer(nbhttp.Config{
-		Network:      "tcp",
-		Addrs:        []string{"localhost:8000", "localhost:8001", "localhost:8002"},
+		Network: "tcp",
+		Addrs: []string{
+			"localhost:28000",
+			"localhost:28001",
+			"localhost:28002",
+			"localhost:28003",
+			"localhost:28004",
+			"localhost:28005",
+			"localhost:28006",
+			"localhost:28007",
+			"localhost:28008",
+			"localhost:28009",
+			"localhost:28010",
+			"localhost:28011",
+			"localhost:28012",
+			"localhost:28013",
+			"localhost:28014",
+			"localhost:28015",
+			"localhost:28016",
+			"localhost:28017",
+			"localhost:28018",
+			"localhost:28019",
+			"localhost:28020",
+			"localhost:28021",
+			"localhost:28022",
+			"localhost:28023",
+			"localhost:28024",
+		},
+		MaxLoad:      1000000,
 		NPoller:      runtime.NumCPU() * 2,
 		NParser:      runtime.NumCPU() * 4,
 		TaskPoolSize: runtime.NumCPU() * 512,
@@ -43,7 +77,11 @@ func main() {
 	ticker := time.NewTicker(time.Second)
 	for i := 1; true; i++ {
 		<-ticker.C
-		fmt.Printf("running for %v seconds, NumGoroutine: %v\n", i, runtime.NumGoroutine())
-		fmt.Println(g.State().String())
+		n := atomic.SwapUint64(&qps, 0)
+		total += n
+		fmt.Printf("running for %v seconds, online: %v, NumGoroutine: %v, qps: %v, total: %v\n", i, g.State().Online, runtime.NumGoroutine(), n, total)
+		// if i%10 == 1 {
+		// 	fmt.Println(g.State().String())
+		// }
 	}
 }
