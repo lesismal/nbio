18fbc4bf15540dbe9dd4bb53a77adbd878ffcddc
lesismal <40462947+lesismal@users.noreply.github.com>
2021-03-08 12:29:32 +0800

    - update http std server example

--- examples/http/stdserver/server.go
+++ examples/http/stdserver/server.go
@@ -5,7 +5,13 @@ import (
 	"io"
 	"net/http"
 	"runtime"
+	"sync/atomic"
 	"time"
+)
+
+var (
+	qps   uint64 = 0
+	total uint64 = 0
 )
 
 func onEcho(w http.ResponseWriter, r *http.Request) {
@@ -15,26 +21,58 @@ func onEcho(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.Write([]byte(time.Now().Format("20060102 15:04:05")))
 	}
+	atomic.AddUint64(&qps, 1)
 }
 
-func serve(addr string) {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/echo", onEcho)
-	server := http.Server{
-		Addr:    addr,
-		Handler: mux,
+func serve(addrs []string) {
+	for _, addr := range addrs {
+		go func() {
+			mux := &http.ServeMux{}
+			mux.HandleFunc("/echo", onEcho)
+			server := http.Server{
+				Addr:    addr,
+				Handler: mux,
+			}
+			server.ListenAndServe()
+		}()
 	}
-	server.ListenAndServe()
 }
 
 func main() {
-	go serve("localhost:9000")
-	go serve("localhost:9001")
-	go serve("localhost:9002")
+	addrs := []string{
+		"localhost:29000",
+		"localhost:29001",
+		"localhost:29002",
+		"localhost:29003",
+		"localhost:29004",
+		"localhost:29005",
+		"localhost:29006",
+		"localhost:29007",
+		"localhost:29008",
+		"localhost:29009",
+		"localhost:29010",
+		"localhost:29011",
+		"localhost:29012",
+		"localhost:29013",
+		"localhost:29014",
+		"localhost:29015",
+		"localhost:29016",
+		"localhost:29017",
+		"localhost:29018",
+		"localhost:29019",
+		"localhost:29020",
+		"localhost:29021",
+		"localhost:29022",
+		"localhost:29023",
+		"localhost:29024",
+	}
+	serve(addrs)
 
 	ticker := time.NewTicker(time.Second)
 	for i := 1; true; i++ {
 		<-ticker.C
-		fmt.Printf("running for %v seconds, NumGoroutine: %v\n", i, runtime.NumGoroutine())
+		n := atomic.SwapUint64(&qps, 0)
+		total += n
+		fmt.Printf("running for %v seconds, NumGoroutine: %v, qps: %v, total: %v\n", i, runtime.NumGoroutine(), n, total)
 	}
 }
