593a3718f34fc4e4eedda52f706c32d27cb0dac5
lesismal <40462947+lesismal@users.noreply.github.com>
2021-03-08 21:28:23 +0800

    - update example

--- examples/http/server/server.go
+++ examples/http/server/server.go
@@ -32,40 +32,8 @@ func main() {
 
 	g := nbhttp.NewServer(nbhttp.Config{
 		Network: "tcp",
-		Addrs: []string{
-			"localhost:28000",
-			"localhost:28001",
-			"localhost:28002",
-			"localhost:28003",
-			"localhost:28004",
-			"localhost:28005",
-			"localhost:28006",
-			"localhost:28007",
-			"localhost:28008",
-			"localhost:28009",
-			"localhost:28010",
-			"localhost:28011",
-			"localhost:28012",
-			"localhost:28013",
-			"localhost:28014",
-			"localhost:28015",
-			"localhost:28016",
-			"localhost:28017",
-			"localhost:28018",
-			"localhost:28019",
-			"localhost:28020",
-			"localhost:28021",
-			"localhost:28022",
-			"localhost:28023",
-			"localhost:28024",
-		},
-		MaxLoad:      1000000,
-		NPoller:      runtime.NumCPU() * 2,
-		NParser:      runtime.NumCPU() * 4,
-		TaskPoolSize: runtime.NumCPU() * 512,
-		TaskIdleTime: time.Second * 120,
-		LockThread:   true,
-	}, mux, nil)
+		Addrs:   []string{"localhost:28000"},
+	}, mux, nil, nil)
 
 	err := g.Start()
 	if err != nil {
@@ -80,8 +48,5 @@ func main() {
 		n := atomic.SwapUint64(&qps, 0)
 		total += n
 		fmt.Printf("running for %v seconds, online: %v, NumGoroutine: %v, qps: %v, total: %v\n", i, g.State().Online, runtime.NumGoroutine(), n, total)
-		// if i%10 == 1 {
-		// 	fmt.Println(g.State().String())
-		// }
 	}
 }
