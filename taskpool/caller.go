f91ea8c04c78ea662966926323e14265b663ca68
lesismal <40462947+lesismal@users.noreply.github.com>
2021-04-12 22:46:36 +0800

    - update comment

--- taskpool/caller.go
+++ taskpool/caller.go
@@ -1,3 +1,7 @@
+// Copyright 2020 lesismal. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package taskpool
 
 import (
