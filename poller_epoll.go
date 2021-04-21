5bd7daa41097c471976c0d4b499c05bf27f5e461
lesismal <40462947+lesismal@users.noreply.github.com>
2021-04-21 13:57:01 +0800

    + NListener config epoll

--- poller_epoll.go
+++ poller_epoll.go
@@ -281,12 +281,14 @@ func newPoller(g *Gopher, isListener bool, index int) (*poller, error) {
 
 	if isListener {
 		if len(g.lfds) > 0 {
-			for _, lfd := range g.lfds {
-				// EPOLLEXCLUSIVE := (1 << 28)
-				if err := syscall.EpollCtl(fd, syscall.EPOLL_CTL_ADD, lfd, &syscall.EpollEvent{Fd: int32(lfd), Events: syscall.EPOLLIN | (1 << 28)}); err != nil {
-					syscall.Close(fd)
-					syscall.Close(int(r0))
-					return nil, err
+			for i, lfd := range g.lfds {
+				if i%len(g.listeners) == index {
+					// EPOLLEXCLUSIVE := (1 << 28)
+					if err := syscall.EpollCtl(fd, syscall.EPOLL_CTL_ADD, lfd, &syscall.EpollEvent{Fd: int32(lfd), Events: syscall.EPOLLIN | (1 << 28)}); err != nil {
+						syscall.Close(fd)
+						syscall.Close(int(r0))
+						return nil, err
+					}
 				}
 			}
 		} else {
