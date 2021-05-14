cc2bc62b042aa98b7b273188f2f0c2ba557b4321
lesismal <40462947+lesismal@users.noreply.github.com>
2021-05-14 17:00:36 +0800

    - use std listener

--- conn_unix.go
+++ conn_unix.go
@@ -481,44 +481,6 @@ func (c *Conn) closeWithErrorWithoutLock(err error) error {
 	return syscall.Close(c.fd)
 }
 
-func dupStdConn(conn net.Conn) (*Conn, error) {
-	sc, ok := conn.(interface {
-		SyscallConn() (syscall.RawConn, error)
-	})
-	if !ok {
-		return nil, errors.New("RawConn Unsupported")
-	}
-	rc, err := sc.SyscallConn()
-	if err != nil {
-		return nil, errors.New("RawConn Unsupported")
-	}
-
-	var newFd int
-	errCtrl := rc.Control(func(fd uintptr) {
-		newFd, err = syscall.Dup(int(fd))
-	})
-
-	if errCtrl != nil {
-		return nil, errCtrl
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = syscall.SetNonblock(newFd, true)
-	if err != nil {
-		syscall.Close(newFd)
-		return nil, err
-	}
-
-	return &Conn{
-		fd:    newFd,
-		lAddr: conn.LocalAddr(),
-		rAddr: conn.RemoteAddr(),
-	}, nil
-}
-
 func newConn(fd int, lAddr, rAddr net.Addr) *Conn {
 	return &Conn{
 		fd:    fd,
