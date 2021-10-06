bf4302ce9c329d26fb984c5900b713aa71e6dcdb
lesismal <wuqiongjishu@163.com>
2021-10-07 00:54:50 +0800

    example: optimize fixedbufferpool

--- examples/websocket_tls/server_manual_frame_assembly/fixedbufferpool.go
+++ examples/websocket_tls/server_manual_frame_assembly/fixedbufferpool.go
@@ -2,41 +2,57 @@ package main
 
 import (
 	"os"
+	"sync"
 	"time"
 )
 
 type Pool struct {
-	buffers    chan []byte
-	getTimeout time.Duration
+	pool        sync.Pool
+	buffers     chan struct{}
+	messageSize int
+	getTimeout  time.Duration
 }
 
 func NewFixedBufferPool(buffers, messageSize int, getTimeout time.Duration) *Pool {
 	rtn := &Pool{
-		buffers:    make(chan []byte, buffers),
-		getTimeout: getTimeout,
+		buffers:     make(chan struct{}, buffers),
+		messageSize: messageSize,
+		getTimeout:  getTimeout,
+		pool: sync.Pool{
+			New: func() interface{} {
+				buf := make([]byte, messageSize)
+				return &buf
+			},
+		},
 	}
 	for i := 0; i < buffers; i++ {
-		rtn.buffers <- make([]byte, 0, messageSize)
+		rtn.buffers <- struct{}{}
 	}
 	return rtn
 }
 
 func (p *Pool) Put(in []byte) {
-	p.buffers <- in
+	if len(in) == p.messageSize {
+		select {
+		case p.buffers <- struct{}{}:
+			p.pool.Put(&in)
+		default:
+		}
+	}
 }
 
 func (p *Pool) Get() ([]byte, error) {
 	// don't wast cycles building a timer if there is a buffer available
 	select {
-	case rtn := <-p.buffers:
-		return rtn[:0], nil
+	case <-p.buffers:
+		return (*(p.pool.Get().(*[]byte)))[0:0], nil
 	default:
 	}
 	t := time.NewTimer(p.getTimeout)
 	select {
-	case rtn := <-p.buffers:
+	case <-p.buffers:
 		t.Stop()
-		return rtn[:0], nil
+		return (*(p.pool.Get().(*[]byte)))[0:0], nil
 	case <-t.C:
 		return nil, os.ErrDeadlineExceeded
 	}
