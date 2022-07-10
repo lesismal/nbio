b7625b69407be6905144adf3fb0ff5fd24a5f46b
zbh255 <565584326@qq.com>
2022-07-10 16:24:26 +0800

    append explain comment to race_disable.go

--- race_disable.go
+++ race_disable.go
@@ -1,11 +1,6 @@
 package nbio
 
 //go:norace
-func cohereSetShutdown(p *poller, b bool) {
-	p.shutdown = b
-}
-
-//go:norace
 func coherePollerRun(g *Engine) {
 	for i := 0; i < g.pollerNum; i++ {
 		g.pollers[i].ReadBuffer = make([]byte, g.readBufferSize)
@@ -15,11 +10,20 @@ func coherePollerRun(g *Engine) {
 }
 
 //go:norace
+// equal (*poller).shutdown = b
+func cohereSetShutdown(p *poller, b bool) {
+	p.shutdown = b
+}
+
+//go:norace
+// equal returns (*poller).shutdown
 func cohereLoadShutdown(p *poller) bool {
 	return p.shutdown
 }
 
 //go:norace
+//	equal if (*Conn) == (*poller).(*Engine).connsUnix[fd]
+//	Is true equal (*poller).(*Engine).connsUnix[fd] = nil;(*poller).deleteEvent(fd)
 func cohereDeleteConnElem(p *poller, fd int, c *Conn) {
 	if c == p.g.connsUnix[fd] {
 		p.g.connsUnix[fd] = nil
@@ -28,26 +32,31 @@ func cohereDeleteConnElem(p *poller, fd int, c *Conn) {
 }
 
 //go:norace
+// equal return (*poller).(*Engine).connsUnix[fd]
 func cohereGetConn(p *poller, fd int) *Conn {
 	return p.g.connsUnix[fd]
 }
 
 //go:norace
+// equal (*poller).(*Engine).connsUnix[fd] = c
 func cohereAddConn(p *poller, fd int, c *Conn) {
 	p.g.connsUnix[fd] = c
 }
 
 //go:norace
+// equal return timerHeap.Len()
 func cohereLenTimers(ts timerHeap) int {
 	return ts.Len()
 }
 
 //go:norace
+// equal timerHeap[start:end]
 func cohereModifyLittleHeap(ts timerHeap, start, end int) timerHeap {
 	return ts[start:end]
 }
 
 //go:norace
+// equal *(*timerHeap) = ts
 func cohereUpdateLittleHeap(ptr *timerHeap, ts timerHeap) {
 	*ptr = ts
 }
