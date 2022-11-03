102f0e9c9d847b728fcb6daa429416a21a94e639
lesismal <lesismal@gmail.com>
2022-11-03 17:30:44 +0800

    limit MaxOpenFiles to avoid slice len range panic

--- net_unix.go
+++ net_unix.go
@@ -15,8 +15,8 @@ import (
 
 func init() {
 	var limit syscall.Rlimit
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err ==nil {
-		if n := int(limit.Max); n > 0 {
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err == nil {
+		if n := int(limit.Max); n > 0 && n < MaxOpenFiles {
 			MaxOpenFiles = n
 		}
 	}
