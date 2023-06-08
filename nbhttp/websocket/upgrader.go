9e3a367fedba47b03d507ea4a07a78330c6ba517
lesismal <lesismal@gmail.com>
2023-06-08 19:35:14 +0800

    save

--- nbhttp/websocket/upgrader.go
+++ nbhttp/websocket/upgrader.go
@@ -164,7 +164,7 @@ func (u *Upgrader) OnOpen(h func(*Conn)) {
 func (u *Upgrader) OnMessage(h func(*Conn, MessageType, []byte)) {
 	if h != nil {
 		u.messageHandler = func(c *Conn, messageType MessageType, data []byte) {
-			if c.Engine.ReleaseWebsocketPayload && len(data) > 0 {
+			if c.isBlockingMod && c.Engine.ReleaseWebsocketPayload && len(data) > 0 {
 				defer c.Engine.BodyAllocator.Free(data)
 			}
 			h(c, messageType, data)
@@ -369,11 +369,7 @@ func (u *Upgrader) Upgrade(w http.ResponseWriter, r *http.Request, responseHeade
 	}
 
 	if wsc.isBlockingMod {
-
 		go wsc.BlockingModReadLoopByReadWriter(rw)
-		// } else {
-		// 	go wsc.BlockingModReadLoop(u.BlockingModReadBufferSize)
-		// }
 	}
 
 	return wsc, nil
