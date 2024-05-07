cf4411773001a240e5298d2124243f3037897127
byene0923 <byene0923@gmail.com>
2024-05-07 17:49:42 +0800

    feat: 1. update outdated comment for MaxWriteBufferSize 2. set DefaultMaxWriteBufferSize 0 by default 3. add negative number check for MaxWriteBufferSize in NewEngine

--- engine_unix.go
+++ engine_unix.go
@@ -270,6 +270,9 @@ func NewEngine(conf Config) *Engine {
 	if conf.ReadBufferSize <= 0 {
 		conf.ReadBufferSize = DefaultReadBufferSize
 	}
+	if conf.MaxWriteBufferSize < 0 {
+		conf.MaxWriteBufferSize = DefaultMaxWriteBufferSize
+	}
 	if conf.MaxConnReadTimesPerEventLoop <= 0 {
 		conf.MaxConnReadTimesPerEventLoop = DefaultMaxConnReadTimesPerEventLoop
 	}
