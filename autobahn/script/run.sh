c66f5a7baf09b7796221e3de872d902a48d1aa5a
mark <mobussun+1@gmail.com>
2025-03-01 19:48:18 +0800

    recover autobahn dir

--- autobahn/script/run.sh
+++ autobahn/script/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# 切换到项目根目录
-cd ../../
-ROOT_DIR=$(pwd)
-
 mkdir -p ./autobahn/bin
 go build -o ./autobahn/bin/autobahn_server ./autobahn/server/
 go build -o ./autobahn/bin/autobahn_reporter ./autobahn/reporter/
@@ -11,14 +7,14 @@ go build -o ./autobahn/bin/autobahn_reporter ./autobahn/reporter/
 echo "pwd:" $(pwd)
 ./autobahn/bin/autobahn_server &
 
-rm -rf ${ROOT_DIR}/autobahn/report
-mkdir -p ${ROOT_DIR}/autobahn/report/
+rm -rf ${PWD}/autobahn/report
+mkdir -p ${PWD}/autobahn/report/
 
 docker pull crossbario/autobahn-testsuite
 
 docker run -i --rm \
-	-v ${ROOT_DIR}/autobahn/config:/config \
-	-v ${ROOT_DIR}/autobahn/report:/report \
+	-v ${PWD}/autobahn/config:/config \
+	-v ${PWD}/autobahn/report:/report \
 	--network host \
 	--name=autobahn \
 	crossbario/autobahn-testsuite \
@@ -28,8 +24,8 @@ trap ctrl_c INT
 ctrl_c() {
 	echo "SIGINT received; cleaning up"
 	docker kill --signal INT "autobahn" >/dev/null
-	rm -rf ${ROOT_DIR}/autobahn/bin
-	rm -rf ${ROOT_DIR}/autobahn/report
+	rm -rf ${PWD}/autobahn/bin
+	rm -rf ${PWD}/autobahn/report
 	cleanup
 	exit 130
 }
@@ -38,6 +34,6 @@ cleanup() {
 	killall autobahn_server
 }
 
-./autobahn/bin/autobahn_reporter ${ROOT_DIR}/autobahn/report/index.json
+./autobahn/bin/autobahn_reporter ${PWD}/autobahn/report/index.json
 
 cleanup
---
+++ autobahn/server/go.mod
@@ -0,0 +1,13 @@
+module server
+
+go 1.24.0
+
+require (
+	github.com/lesismal/llib v1.2.1
+	github.com/lesismal/nbio v1.6.2
+)
+
+require (
+	golang.org/x/crypto v0.0.0-20210513122933-cd7d49e622d5 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
